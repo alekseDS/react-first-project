e04121741365db03ff595cdd421535cf9ac6612a
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-11-02 17:55:48 +0100

    сделал форму контролируемой

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,33 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('')
+  const [pass, setPass] = useState('')
+
+  const handleLogin = ()=>{
+    setData(Date.now())
+  }
+
+  const handleChangeLogin = (e)=>{
+    setData(e.target.value)
+  }
+
+  const handleChangePass = (e)=>{
+    setPass(e.target.value)
+  }
 
   return (
       <Stack alignItems={"center"}>
         <Stack width={"350px"} gap={2}>
           <Typography variant="h3" gutterBottom>
-            Вход в сервис
+            Вход в сервис {data}
           </Typography>
-          <TextField id="standard-basic" label="Login" variant="standard" />
-          <TextField id="standard-basic" label="Password" variant="standard" type="password" />
-          <Button variant="contained">Войти</Button>
+          <TextField id="login" label="Login" variant="standard" onChange={handleChangeLogin} value={data} />
+          <TextField id="password" label="Password" variant="standard" type="password" onChange={handleChangePass} value={pass} />
+          <Button variant="contained" onClick={handleLogin}>Войти</Button>
         </Stack>
       </Stack>
   )
