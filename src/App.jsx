a92e81147c04a4c66237a0b7fa425f685db48449
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-11-02 17:31:24 +0100

    добавил форму логинизации

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 function App() {
 
   return (
-      <div>
-        <Button variant="contained">Войти</Button>
-      </div>
+      <Stack alignItems={"center"}>
+        <Stack width={"350px"} gap={2}>
+          <Typography variant="h3" gutterBottom>
+            Вход в сервис
+          </Typography>
+          <TextField id="standard-basic" label="Login" variant="standard" />
+          <TextField id="standard-basic" label="Password" variant="standard" type="password" />
+          <Button variant="contained">Войти</Button>
+        </Stack>
+      </Stack>
   )
 }
 
